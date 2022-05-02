2e816bd4fc51415ec998ff939bb151aacc02d0dd
Daniil Pryima <swimmin2@gmail.com>
2022-05-02 11:43:07 +0300

    feat: Add library socket patcher

---
+++ Patch-Library-Socket.ps1
@@ -0,0 +1,84 @@
+param(
+  [Parameter(Mandatory=$true)] [string] $InputFile,
+  [Parameter(Mandatory=$true)] [string] $OutputFile,
+  [string] $FFDec = 'C:\Program Files (x86)\FFDec\ffdec.jar',
+  [string] $GameServer,
+  [string] $ResourceServer
+)
+
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+Write-Output ">>> Patching $InputFile -> $OutputFile"
+
+if($PSBoundParameters.ContainsKey('GameServer')) {
+  $GameServerAddress, $GameServerPort = $GameServer.Split(':')
+  if($null -Eq $GameServerPort || $null -Eq $GameServerPort) {
+    Write-Error ">>> Invalid game server endpoint: $GameServer"
+    return
+  }
+
+  Write-Output ">>> Game server: ${GameServerAddress}:${GameServerPort}"
+}
+
+if($PSBoundParameters.ContainsKey('ResourceServer')) {
+  Write-Output ">>> Resource server: $ResourceServer"
+}
+
+$TemporaryDirectory = New-TemporaryDirectory
+Write-Output ">>> Temporary directory: $TemporaryDirectory"
+
+java -jar $FFDec -selectclass Game -format script:pcode -export script $TemporaryDirectory $InputFile
+
+$ScriptLocation = Join-Path $TemporaryDirectory 'scripts' 'Game.pcode'
+$Script = Get-Content $ScriptLocation -Raw
+
+$MethodStartString = 'trait method QName(PackageNamespace(""),"activateAllModels")'
+$MethodEndString = 'end ; method'
+
+$MethodStart = $Script.IndexOf($MethodStartString)
+$MethodEnd = $Script.IndexOf($MethodEndString, $MethodStart)
+
+$Method = $Script.Substring($MethodStart, ($MethodEnd + $MethodEndString.Length) - $MethodStart)
+
+if($PSBoundParameters.ContainsKey('GameServer')) {
+  $OldMethod = $Method
+  $Method = $Method -Replace '(findpropstrict\s+QName\(PackageNamespace\("alternativa\.startup"\),"ConnectionParameters"\)\r?\n\s*)pushstring\s+".+"()', "`${1}pushstring `"$GameServerAddress`" ; changed`${2}"
+  if($Method -Ceq $OldMethod) {
+    Write-Error ">>> Failed to change game server address to $GameServerAddress"
+  } else {
+    Write-Output ">>> Changed game server address to $GameServerAddress"
+  }
+
+  $OldMethod = $Method
+  $Method = $Method -Replace '(getglobalscope\r?\n\s*)pushshort\s+\d+()', "`${1}pushint $GameServerPort ; changed`${2}"
+  if($Method -Ceq $OldMethod) {
+    Write-Error ">>> Failed to change game server port to $GameServerPort"
+  } else {
+    Write-Output ">>> Changed game server port to $GameServerPort"
+  }
+}
+
+if($PSBoundParameters.ContainsKey('ResourceServer')) {
+  $OldMethod = $Method
+  $Method = $Method -Replace '(call\s+1\r?\n\s*pushnull\r?\n\s*)pushstring\s+".+"()', "`${1}pushstring `"$ResourceServer`" ; changed`${2}"
+  if($Method -Ceq $OldMethod) {
+    Write-Error ">>> Failed to change resource server URL to $ResourceServer"
+  } else {
+    Write-Output ">>> Changed resource server URL to $ResourceServer"
+  }
+}
+
+Set-Content -Path $ScriptLocation $Method
+# Write-Output $Method
+
+Write-Output ">>> Patching SWF file..."
+java -jar $FFDec -replace $InputFile $OutputFile Game $ScriptLocation 1
+
+Write-Output ">>> Removing temporary directory..."
+Remove-Item -Recurse -Force $TemporaryDirectory
+
+Write-Output ">>> Done! Output file: $OutputFile"
--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 
 A bunch of utilities for ProTanki.
 
+## [Patch-Library-Socket.ps1](Patch-Library-Socket.ps1)
+
+PowerShell script that patches `library.swf` (`http://s2.protanki-online.com/library.swf`) to use custom game and/or resource server.  
+Requires [JPEXS Free Flash Decompiler](https://github.com/jindrapetrik/jpexs-decompiler) to be installed.
+The minimum supported version is `library-20220501-221826`.
+
+| Argument         | Type      | Description                                                      | Default value                            |
+|------------------|-----------|------------------------------------------------------------------|------------------------------------------|
+| `InputFile`      | `string`  | Game SWF to patch                                                |                                          |
+| `OutputFile`     | `string`  | Patched SWF file                                                 |                                          |
+| `FFDec`          | `string`  | Path to `ffdec.jar` file                                         | `C:\Program Files (x86)\FFDec\ffdec.jar` |
+| `GameServer`     | `string?` | Game server endpoint in `IP:port` format (e.g. `127.0.0.1:1337`) |                                          |
+| `ResourceServer` | `string?` | Resource server URL (e.g. `http://127.0.0.1:8080/resource`)      |                                          |
+
+Example usage (PowerShell):
+```powershell
+# Change game server
+./Patch-Library-Socket -InputFile 'library-original.swf' -OutputFile 'library.swf' -GameServer '127.0.0.1:1337'
+
+# Change resource server
+./Patch-Library-Socket -InputFile 'library-original.swf' -OutputFile 'library.swf' -ResourceServer 'http://127.0.0.1:8080/resource'
+
+# Change both game and resource server
+./Patch-Library-Socket -InputFile 'library-original.swf' -OutputFile 'library.swf' -GameServer '127.0.0.1:1337' -ResourceServer 'http://127.0.0.1:8080/resource'
+
+# Use custom ffdec.jar
+./Patch-Library-Socket -InputFile 'library-original.swf' -OutputFile 'library.swf' -FFDec 'ffdec.jar' -GameServer '127.0.0.1:1337'
+```
+
 ## [packet-log.patch.txt](packet-log.patch.txt)
 
 Patch (manual applying via [JPEXS Free Flash Decompiler](https://github.com/jindrapetrik/jpexs-decompiler)) to log incoming and outgoing game packets.  
